180fcd8491cb4f56a7a02061c9e78dca015e1012
billy1103 <billy.0311@hotmail.com>
2022-03-24 00:57:38 +0000

    initial

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Light(props) {
+	return (
+		<div>
+			<div class="trafficTop">hello</div>
+			<div class="container">
+				<div class="red light"></div>
+				<div class="yellow light"></div>
+				<div class="green light"></div>
+			</div>
+		</div>
+	);
+}
