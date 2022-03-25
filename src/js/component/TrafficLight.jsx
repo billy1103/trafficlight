1c7a9c2378cd64b8121ee397bf8a1cc176f017cf
billy1103 <billy.0311@hotmail.com>
2022-03-25 19:18:34 +0000

    second

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Light(props) {
+	const [color, setColor] = useState("onClick");
 	return (
 		<div>
-			<div class="trafficTop">hello</div>
-			<div class="container">
-				<div class="red light"></div>
-				<div class="yellow light"></div>
-				<div class="green light"></div>
+			<div className="top"></div>
+			<div className="container">
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"red light" + (color === "red" ? " selected" : "")
+					}></div>
+				<div
+					onClick={() => setColor("orange")}
+					className={
+						"orange light" + (color === "orange" ? " selected" : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"green light" + (color === "green" ? " selected" : "")
+					}></div>
 			</div>
 		</div>
 	);
